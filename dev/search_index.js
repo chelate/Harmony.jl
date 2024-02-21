2bccf6d62ca3609b2c93194c4b70195156f55c7d
Documenter.jl <documenter@juliadocs.github.io>
2024-02-21 14:21:49 +0000

    build based on 25af164

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Harmony","category":"page"},{"location":"#Harmony","page":"Home","title":"Harmony","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Harmony.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Harmony]","category":"page"}]
+}
